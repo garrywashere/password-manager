8ff94bedc48652ebbdd21751887935bb034ddd2c
Garry <github@garrynet.co.uk>
2024-02-12 14:49:19 +0000

    launch menu to main

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+from InquirerPy import inquirer as inq
+from InquirerPy.separator import Separator
 from src import frontend
+import os
 
+clear = lambda: os.system("clear") if os.name == "posix" else os.system("cls")
 
-frontend.app.run(host="127.0.0.1", port=8080, debug=True)
+def main():
+    running = True
+    while running:
+        clear()
+        print("Welcome to Password Manager.\n")
+        choices = ["Start server", Separator(), "Exit"]
+        choice = inq.select(message="Make a selection", choices=choices).execute()
+        match choice:
+            case "Start server":
+                frontend.app.run(host="127.0.0.1", port=8080, debug=True)
+            case "Exit":
+                running = False
+                exit()
+
+if __name__ == "__main__":
+    try:
+        main()
+    # except Exception as e:
+    #     print("Error:", e)
+    except KeyboardInterrupt:
+        print("Exiting")
+        exit()
