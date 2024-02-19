46dadda8432624272eb50c59df89833d0eb6554d
Garry <github@garrynet.co.uk>
2024-02-19 02:19:39 +0000

    fixed ./data issue

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
-# from InquirerPy import inquirer as inq
-# from InquirerPy.separator import Separator
 from src import frontend
 import os
 
 clear = lambda: os.system("clear") if os.name == "posix" else os.system("cls")
 
-# def main():
-#     running = True
-#     while running:
-#         clear()
-#         print("Welcome to Password Manager.\n")
-#         choices = ["Start server", Separator(), "Exit"]
-#         choice = inq.select(message="Make a selection", choices=choices).execute()
-#         match choice:
-#             case "Start debug server":
-#                 frontend.app.run(host="127.0.0.1", port=8080, debug=True)
-#             case "Exit":
-#                 running = False
-#                 exit()
+if __name__ == "__main__":
+    clear()
 
-# if __name__ == "__main__":
-#     try:
-#         main()
-#     except Exception as e:
-#         print("Error:", e)
-#     except KeyboardInterrupt:
-#         print("Exiting")
-#         exit()
+    if not os.path.exists("./data"):
+        os.mkdir("./data")
 
-frontend.app.run(host="127.0.0.1", port=8080, debug=True)
+    frontend.app.run(host="127.0.0.1", port=8080, debug=True)
