aa48d5bdc6b8cfc77962afa52758d1e212a02bc6
Garry <github@garrynet.co.uk>
2024-03-13 23:30:46 +0000

    changed main.py - host addr

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.frontend import app
 import os
 
 if __name__ == "__main__":
-    HOST = "0.0.0.0"
+    HOST = "127.0.0.1"
     PORT = 8080
 
     clear = lambda: os.system("clear") if os.name == "posix" else os.system("cls")
